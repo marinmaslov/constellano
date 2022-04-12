de37f7e50f892cfa29177ebfaf8c9e1f55735a4f
Marin Maslov <marin.maslov@ericsson.com>
2022-04-12 19:12:33 +0200

    Adding logger for Heroku

--- constellano.py
+++ constellano.py
@@ -1,6 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
+import sys
+import logging
 
 app = Flask(__name__)
+app.logger.addHandler(logging.StreamHandler(sys.stdout))
+app.logger.setLevel(logging.ERROR)
 
 @app.route('/')
 def constellano():
