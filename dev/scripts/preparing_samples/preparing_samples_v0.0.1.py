0b576facec849d963d59b51b5569d283c237b843
Marin Maslov <marinmaslov@Air-od-Marin.fritz.box>
2022-04-18 17:46:41 +0200

    Preparation script fixes

--- dev/scripts/preparing_samples/preparing_samples_v0.0.1.py
+++ dev/scripts/preparing_samples/preparing_samples_v0.0.1.py
@@ -100,7 +100,7 @@ def main(argv):
     counter = 0
     for file in os.listdir(positives_dir):
         if file.endswith(".jpg"):
-            command = "opencv_createsamples -img " + str(samples_dir + file) + " -bg negatives.txt " + "-info " + str(samples_dir) + "/samples_" + str(counter) + ".txt -pngoutput " + str(samples_dir) + "/samples_" + str(counter) + \
+            command = "opencv_createsamples -img " + str(samples_dir + file) + " -bg negatives.txt " + "-info " + str(samples_dir) + "samples_" + str(counter) + ".txt -pngoutput " + str(samples_dir) + "samples_" + str(counter) + \
                 " -maxxangle " + str(maxxangle) + " -maxyangle " + str(maxyangle) + " -maxzangle " + str(maxzangle) + " -num " + str(number_of_samples) + \
                 " -maxidev " + str(maxidev) + " -w " + \
                 str(width) + " -h " + str(height)
