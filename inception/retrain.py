0ba3e9b81a9753057b22645c488107b17b1b88ac
Marin Maslov <marin.maslov@ericsson.com>
2022-05-11 12:53:50 +0200

    Creating retrain.py script

---
+++ inception/retrain.py
@@ -0,0 +1,33 @@
+import itertools
+import os
+
+import matplotlib.pylab as plt
+import numpy as np
+
+import tensorflow as tf
+import tensorflow_hub as hub
+
+print("TF version:", tf.__version__)
+print("Hub version:", hub.__version__)
+print("GPU is", "available" if tf.config.list_physical_devices('GPU') else "NOT AVAILABLE")
+
+model_name = "inception_v3"
+
+model_handle_map = { "inception_v3": "https://tfhub.dev/google/imagenet/inception_v3/feature-vector/4" }
+
+model_image_size_map = { "inception_v3": 299 }
+
+model_handle = model_handle_map.get(model_name)
+pixels = model_image_size_map.get(model_name, 224)
+
+print(f"Selected model: {model_name} : {model_handle}")
+
+IMAGE_SIZE = (pixels, pixels)
+print(f"Input size {IMAGE_SIZE}")
+
+BATCH_SIZE = 16#@param {type:"integer"}
+
+data_dir = tf.keras.utils.get_file(
+    'flower_photos',
+    'https://storage.googleapis.com/download.tensorflow.org/example_images/flower_photos.tgz',
+    untar=True)
